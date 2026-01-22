29fac636c7c0b9c6dadec3c3a6a3b9610dbd26fc
Dobromir Zahariev <dzahariev@yahoo.com>
2026-01-22 18:32:01 +0200

    Serve static content only, remove nodejs runtime

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
-}
+};
 
 export default nextConfig;
